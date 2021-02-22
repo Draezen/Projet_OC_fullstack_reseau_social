4b32373c089a231afa1dfca186417c5c3ad75978
Thomas <t.danger@hotmail.fr>
2021-02-22 16:17:03 +0100

    essais connexion bdd

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require("express")
 //importer body-parser pour gérer le corps de la demande
 const bodyParser = require("body-parser")
 
+
 //package node pour trouver le chemin d'un ficher/dossier
 const path = require("path")
 
@@ -15,26 +16,11 @@ const commentsRoutes = require("./routes/comments")
 //gestion des erreurs 404
 const error404 = require("./middleware/errors")
 
-//connection to mySQL database
-const mysql = require("mysql")
-
-const connection = mysql.createConnection({
-    host : "localhost",
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
-})
-
-connection.connect((err) => {
-    if (err){
-        console.error("error connecting : " + err.stack)
-        return
-    }
-    console.log("Connected as id " + connection.threadId)
-})
-
 //créer l'application express
 const app = express()
+
+//connection to mySQL database
+const mysqlConnection = require("./mysql_connection")
 
 app.use((req, res, next) => {
     //permet d'accéder à l'api de n'importe quelle origin
@@ -54,10 +40,10 @@ app.use(bodyParser.json());
 app.use("/images", express.static(path.join(__dirname, "images")))
 
 //utilisation du router
-app.use("api/auth", authRoutes)
-app.use("api/user", userRoutes)
-app.use("api/articles", articlesRoutes)
-app.use("api/comments", commentsRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userRoutes)
+app.use("/api/articles", articlesRoutes)
+app.use("/api/comments", commentsRoutes)
 
 //gestion des erreurs 404
 app.use(error404)
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,15 @@
 //in controllers
+const mysqlConnection = require("../mysql_connection")
 
 exports.signup = (req, res, next) => {
-
+    mysqlConnection.query("INSERT INTO users VALUES (NULL, '2thomas@google.com', 'mdp', 'nomDeFamille', 'prenom', 1, 'a12z2e3r5t8')", (err, results, fields) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.status(201).json({ message : "User created" });
+        }
+    })
+    mysqlConnection.end()
 }
 
 exports.login = (req, res, next) => {
